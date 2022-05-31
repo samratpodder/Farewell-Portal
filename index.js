e4fa31b0216920d677be94331a2bd3225927b1a8
samratpodder <iamsamrat@outlook.com>
2022-05-31 20:11:53 +0530

    Added port variable to index.js

--- index.js
+++ index.js
@@ -258,4 +258,4 @@ app.get('/', (req, res) => {
 
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log('Portal app listening on port 3000!'));
+app.listen(port, () => console.log('Portal app listening at port ' + port));
